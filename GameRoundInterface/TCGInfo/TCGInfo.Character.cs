6852f8f451d3edfdb18002abcd7f170b814ec4b9
HuTao今天吃什么 <1184726415@qq.com>
2023-03-03 22:48:53 +0800

    v0.0.3.0.8

--- GameRoundInterface/TCGInfo/TCGInfo.Character.cs
+++ GameRoundInterface/TCGInfo/TCGInfo.Character.cs
@@ -1,35 +1,54 @@
 ﻿namespace TCGInfo
 {
     /// <summary>
-    /// 在场角色的全部信息
+    /// 在场角色的可变信息
     /// </summary>
-    public struct CharacterInfo
+    public class CharacterInfo
     {
-        public string CharacterID { get; }
-        public int MaxMp { get; }
+        public CharacterInfo(int hp, int mp, string weapon, string artifact, string nature, string[] effects, string[] skills)
+        {
+            Hp = hp;
+            Mp = mp;
+            Weapon = weapon;
+            Artifact = artifact;
+            Nature = nature;
+            Effects = effects;
+            Skills = skills;
+        }
+
         public int Hp { get; set; }
         public int Mp { get; set; }
         public string Weapon { get; set; }
         public string Artifact { get; set; }
         public string Nature { get; set; }
-        public string Tags { get; }
         /// <summary>
         /// 若干个EffectInfo
         /// </summary>
         public string[] Effects { get; set; }
-        public string[] Skills { get; set; }
         /// <summary>
-        /// 角色技能的Info
+        /// 可能受到减费等影响的技能
         /// </summary>
-        public struct SkillInfo
+        public string[] Skills { get; set; }
+    }
+    /// <summary>
+    /// 角色技能的Info
+    /// </summary>
+    public class SkillInfo
+    {
+        public string SkillID { get; }
+        public bool SameDice { get; }
+        public byte DiceType { get; }
+        /// <summary>
+        /// 经过buff计算后的骰子需求
+        /// </summary>
+        public int[] Dices { get; set; }
+        public SkillInfo(string skillID, bool sameDice, byte diceType, int[] dices)
         {
-            public string SkillID { get; }
-            public bool SameDice { get; }
-            public byte DiceType { get; }
-            /// <summary>
-            /// 经过buff计算后的骰子需求
-            /// </summary>
-            public int[] Dices { get; set; }
+            SkillID = skillID;
+            SameDice = sameDice;
+            DiceType = diceType;
+            Dices = dices;
         }
     }
+
 }
---
+++ GameRoundInterface/TCGInfo/TCGInfo.Readonly.cs
@@ -0,0 +1,28 @@
+﻿namespace TCGInfo
+{
+    /// <summary>
+    /// 不会改变的信息
+    /// 只请求一次，实际上是一种简单优化的行为(?这种低能东西真的需要优化吗?)
+    /// </summary>
+    public class CardReadonlyInfo
+    {
+        public CardReadonlyInfo(string iD, string tags)
+        {
+            ID = iD;
+            Tags = tags;
+        }
+
+        public string ID { get; }
+        public string Tags { get; }
+    }
+    public class CharacterReadonlyInfo : CardReadonlyInfo
+    {
+        public CharacterReadonlyInfo(string iD, string tags, int maxMp, string[] skills) : base(iD, tags)
+        {
+            MaxMp = maxMp;
+            Skills = skills;
+        }
+        public int MaxMp { get; }
+        public string[] Skills { get; set; }
+    }
+}
